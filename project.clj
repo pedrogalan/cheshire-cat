2868c4ac449bdbdf65d7d42d23f824814cbcaf36
Pedro Galan <pedro.galan@piksel.com>
2020-04-09 18:22:29 +0100

    Create project

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject cheshire-cat "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :repl-options {:init-ns cheshire-cat.handler}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [proto-repl "0.3.1"]
+                 [cheshire "5.10.0"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler cheshire-cat.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.2"]]}})
